dd1ffe5110ea0fa3ea8d7a2869eeabbbc8834420
armancj <armancanjer@gmail.com>
2023-03-03 16:51:07 -0500

    adding implicit convert number

--- src/common/dto/get-all-query.dto.ts
+++ src/common/dto/get-all-query.dto.ts
@@ -1,14 +1,17 @@
 import { DefaultValue } from '../enums/default-value.enum';
 import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class GetAllQueryDto {
   @IsOptional()
   @IsNumber()
   @Min(0)
+  @Type(() => Number)
   skip?: number = DefaultValue.Skip;
 
   @IsOptional()
   @IsNumber()
   @IsPositive()
+  @Type(() => Number)
   take?: number = DefaultValue.Limit;
 }
